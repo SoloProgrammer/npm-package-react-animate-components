6928c359aaf4d0090afe9233b6d69aba9c64dba1
SoloProgrammer <prathamshinde987@gmail.com>
2024-03-19 20:47:28 +0530

    Created ProgressBar Scroll based component

---
+++ src/animations/Scroll-Effects/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,26 @@
+"use client"
+import * as React from "react";
+import { motion, useScroll } from "framer-motion";
+import { ProgressBarProps } from "../../../types/types";
+
+const ProgressBar = ({ bg, h, position = "top", origin = "left" }: ProgressBarProps) => {
+  const { scrollYProgress } = useScroll();
+
+  return (
+    <motion.div
+      style={{
+        background: bg,
+        height: h,
+        position: "fixed",
+        width: `100vw`,
+        [position]: "0",
+        left: "0",
+        zIndex: "1300",
+        scaleX: scrollYProgress,
+        transformOrigin: origin,
+      }}
+    />
+  );
+};
+
+export { ProgressBar };
